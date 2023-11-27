a313018cb7fb22e56b9f825b44faf2b5f276bd24
Anass El Omari <anas.iramol@gmail.com>
2023-11-27 09:44:10 -0800

    fixing easter egg

--- 0x00-python-hello_world/module_0.py
+++ 0x00-python-hello_world/module_0.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-zen = """
-The Zen of Python, by Tim Peters
+zen = """The Zen of Python, by Tim Peters
 
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -20,7 +19,5 @@ Now is better than never.
 Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
-"""
-
+Namespaces are one honking great idea -- let's do more of those!"""
 print(zen)
