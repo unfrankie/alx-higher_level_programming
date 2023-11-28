e50983f28b06d3aaff413a550375a8503779922f
Anass El Omari <anas.iramol@gmail.com>
2023-11-28 06:04:09 -0800

    README.md - Positive anything is better than negative nothing - the last digit - I sometimes suffer from insomnia. And when I cant fall asleep, I play what I call the alphabet game - When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for alphabet in range(26):
+    print("{:c}".format(alphabet + 97), end="")
---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+for alphabet in range(26):
+    if chr(character + 97) != 'e' and chr(character + 97) != 'q':
+        print("{:c}".format(character + 97), end="")
