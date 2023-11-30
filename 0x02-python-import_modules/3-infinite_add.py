45993f56aaa5387323ad6e804e7cd18a698acaf6
Anass El Omari <anas.iramol@gmail.com>
2023-11-30 08:06:41 -0800

    fixing task 1 & 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -4,5 +4,5 @@ if __name__ == "__main__":
     arg = len(sys.argv)
     sum = 0
     for i in range(1, arg):
-       sum += int(sys.argv[i])
-    print("{}".format(sum))
+        sum += int(sys.argv[i])
+    print("{}".format(sum)))
