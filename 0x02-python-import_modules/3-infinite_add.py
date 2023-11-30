858c39f5afdf2ba1e57051f4bac6778ab95400c4
Anass El Omari <anas.iramol@gmail.com>
2023-11-30 08:09:32 -0800

    fixing task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-import sys
 if __name__ == "__main__":
-    arg = len(sys.argv)
+    import sys
     sum = 0
-    for i in range(1, arg):
+    for i in range(1, len(sys.argv)):
         sum += int(sys.argv[i])
-    print("{}".format(sum)))
+    print(sum)
