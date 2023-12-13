98e2b0fda79c4ddd2bc0e5ed642ebbae278b6b03
Anass El Omari <anas.iramol@gmail.com>
2023-12-13 03:44:49 -0800

    reset task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,22 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    roman_dict = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
-                  'C': 100, 'D': 500, 'M': 1000}
-    nums = []
-    chif = 0
-
     if not roman_string or type(roman_string) is not str:
         return 0
-    else:
-        for elem in roman_string:
-            nums.append(roman_dict[elem])
-
-        for num in nums:
-            if len(nums) == 1:
-                chif = num
-            elif nums[-1] < nums[0]:
-                chif += num
-            elif nums[-1] > nums[0]:
-                subs = sum(nums[:-1])
-                chif = nums[-1] - subs
-        return chif
+    r_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    result = 0
+    for i in range(len(roman_string)):
+        value_0 = r_n[roman_string[i]]
+        value_1 = r_n[roman_string[i + 1]] if i + 1 < len(roman_string) else 0
+        if value_0 < value_1:
+            result -= value_0
+        else:
+            result += value_0
+    return result
