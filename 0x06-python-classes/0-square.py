36d3984dfc43083aefa20da91a65903522ec3dcd
Anass El Omari <anas.iramol@gmail.com>
2023-12-19 04:41:32 -0800

    directory - README file - main test files - my first square - square with size

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
+'''class defining'''
 class Square:
+	'''class'''
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+'''defining square class'''
 class Square:
+	'''square class & size'''
     def __init__(self, size):
         self.__size = size
