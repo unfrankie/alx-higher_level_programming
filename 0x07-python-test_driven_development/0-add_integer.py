99a8ea4e39316af8a1fff2a93e32cde5d04f4df0
Anass El Omari <anas.iramol@gmail.com>
2024-01-03 06:24:49 -0800

    create directory - create readme file - Integers addition - main files

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''add_integer module'''
+
+
+def add_integer(a, b=98):
+    """
+    Adds two integers.
+        a: 1st first number.
+        b: 2nd number default is 98.
+    Returns:
+        int: The sum of a and b.
+    Raises:
+        TypeError: If a or b is not an integer or float.
+    """
+
+
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+
+    return int(a) + int(b)
+
+if __name__ == "__main__":
+    import doctest
+    doctest.testfile("tests/0-add_integer.txt")
