57c78a529090c4ca0cbdf9f2492b69436061af68
Anass El Omari <anas.iramol@gmail.com>
2024-01-10 07:24:08 -0800

    7- Lazy matrix multiplication + test file

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -10,4 +10,13 @@ def lazy_matrix_mul(m_a, m_b):
     multiplie m_a and m_b using matmul
     return result
     """
+    if not all(isinstance(row, list) for row in m_a) or not \
+     all(isinstance(row, list) for row in m_b):
+        raise TypeError("m_a must be a list of lists or m_b \
+         must be a list of lists")
+
+    if not all(isinstance(num, (int, float)) for row in m_a for num in row):
+        raise TypeError("m_a should contain only integers or floats")
+    if not all(isinstance(num, (int, float)) for row in m_b for num in row):
+        raise TypeError("m_b should contain only integers or floats")
     return numpy.matmul(m_a, m_b)
