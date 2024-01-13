6c70c34e6e1b770e4b8009035481068b2ceda3ac
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-01-13 13:55:22 +0100

    fixing update func rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -39,7 +39,7 @@ class Rectangle(Base):
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
 
-    def update(self, *args):
+    def update(self, *args, **kwargs):
         """update the Rectangle instance attributes"""
         if args:
             attrs = ["id", "width", "height", "x", "y"]
