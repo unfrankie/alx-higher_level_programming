485475897eb4e41b423639fa0d3e4bf9af8550e3
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-01-15 17:04:19 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -19,7 +19,7 @@ class Base:
     def to_json_string(list_dictionaries):
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
-        return json.dumps(list_dictionaries)
+        return json.dumps(list_dictionaries, sort_keys=True)
 
     @classmethod
     def from_json_string(cls, json_string):
