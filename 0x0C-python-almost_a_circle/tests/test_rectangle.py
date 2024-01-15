74fb221b75452871fd7b811ae3ab11173f35a329
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-01-15 16:31:34 +0100

    Update test_rectangle.py

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -2,6 +2,7 @@ import unittest
 from unittest.mock import patch
 import io
 from models.rectangle import Rectangle
+from models.base import Base
 
 class TestRectangle(unittest.TestCase):
     def test_area(self):
