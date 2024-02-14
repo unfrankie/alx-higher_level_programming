02c633079ab70c9dbe334d2570bb71e4561b1c03
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-02-14 16:46:46 +0100

    fixing 12-no_genre.sql

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,6 +1,6 @@
 -- Script to list shows without any genre linked
 SELECT tv_shows.title, tv_show_genres.genre_id
 FROM tv_shows
-LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 WHERE tv_show_genres.genre_id IS NULL
-ORDER BY tv_shows.title;
+ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
