8dc3d1188f4984d6279ed56fbfa178e3925bf4b4
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-02-14 16:54:37 +0100

    Update 15-comedy_only.sql

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,8 @@
 -- Script to list Comedy shows
 SELECT tv_shows.title
 FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
+JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 JOIN tv_genres ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_genres.name = 'Comedy'
-ORDER BY tv_shows.title;
+GROUP BY tv_shows.title
+ORDER BY tv_shows.title ASC;
