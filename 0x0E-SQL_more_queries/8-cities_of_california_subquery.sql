af240e1249fa7788bdbab441404f10e8fcd6c050
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-02-14 16:38:14 +0100

    fixing 8-cities_of_california_subquery.sql

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,2 +1,2 @@
 -- Script to list all cities of California using subquery
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California');
+SELECT id, name FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id;
