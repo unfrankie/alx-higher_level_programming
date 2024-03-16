f8641935622314d28bf30045d51477e3808d1cfe
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-16 22:26:21 +0000

    fix 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -3,17 +3,20 @@
 Script that prints the State object with the name passed as argument
 from the database hbtn_0e_6_usa
 """
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
     url_database = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
-    engine = create_engine(url_database)
+    engine = create_engine(url_database, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    state = session.query(State).filter(
+        State.name == sys.argv[4]
+    ).order_by(State.id).first()
     if state:
         print(state.id)
     else:
