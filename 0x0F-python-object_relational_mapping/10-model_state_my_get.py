63e53e4c1192e251f3d7fb03bed6337a530bf0da
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-16 22:29:02 +0000

    Update 10-model_state_my_get.py

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     Session = sessionmaker(bind=engine)
     session = Session()
     state = session.query(State).filter(
-        State.name == sys.argv[4]
+        State.name == argv[4]
     ).order_by(State.id).first()
     if state:
         print(state.id)
