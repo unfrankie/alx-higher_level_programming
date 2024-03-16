ad050bbfb363a7a11282b6182ebe4c43640846aa
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-03-16 22:33:46 +0000

    fix 12-model_state_update_id_2.py

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -3,14 +3,15 @@
 Script that changes the name of a State object
 from the database hbtn_0e_6_usa
 """
-import sys
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
 
 if __name__ == "__main__":
     url_database = f"mysql+mysqldb://{argv[1]}:{argv[2]}@localhost/{argv[3]}"
-    engine = create_engine(url_database)
+    engine = create_engine(url_database, pool_pre_ping=True)
+    Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     state_to_update = session.query(State).filter(State.id == 2).first()
