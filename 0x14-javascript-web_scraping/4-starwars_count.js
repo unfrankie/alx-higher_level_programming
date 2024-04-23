d9b503d1de26347a3150f3cd79af61031cf34c05
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-23 22:44:18 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -8,18 +8,15 @@ let count = 0;
 
 request.get(url, (error, response, body) => {
   if (error) {
-    console.error('Error:', error);
+    console.error(error);
     return;
   }
 
-  try {
-    const data = JSON.parse(body);
-    count = data.results.reduce((total, film) => {
-      const characterFound = film.characters.some(character => character.includes(characterId));
-      return total + (characterFound ? 1 : 0);
-    }, 0);
-    console.log('Number of movies:', count);
-  } catch (parseError) {
-    console.error('Parsing error:', parseError);
-  }
+  const data = JSON.parse(body);
+  count = data.results.reduce((total, film) => {
+    const hasCharacter = film.characters.some(character => character.includes(characterId));
+    return total + (hasCharacter ? 1 : 0);
+  }, 0);
+
+  console.log(count);
 });
