8a2e179c5498da1ec9ead73310480e2d7168bd3d
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-23 22:31:44 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,16 +1,21 @@
 #!/usr/bin/node
 
-const axios = require('axios');
+const request = require('request');
 
 const apiUrl = process.argv[2];
 const characterId = 18;
 
-axios.get(apiUrl)
-  .then(response => {
-    const films = response.data.results;
-    const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
-    console.log(count);
-  })
-  .catch(error => {
+request(apiUrl, (error, response, body) => {
+  if (error) {
     console.error(error);
-  });
+    return;
+  }
+  const films = JSON.parse(body).results;
+  const count = films.reduce((acc, film) => {
+    if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
+      return acc + 1;
+    }
+    return acc;
+  }, 0);
+  console.log(count);
+});
