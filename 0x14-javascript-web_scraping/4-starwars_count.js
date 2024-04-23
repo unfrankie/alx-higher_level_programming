e020c6d7a99dbea437820f8d118b48435fad193b
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-04-23 22:42:52 +0100

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,21 +1,25 @@
 #!/usr/bin/node
 
 const request = require('request');
+const url = process.argv[2];
+const characterId = '18';
 
-const apiUrl = process.argv[2];
-const characterId = 18;
+let count = 0;
 
-request(apiUrl, (error, response, body) => {
-  if (!error) {
-    const films = JSON.parse(body).results;
-    let count = 0;
-    films.forEach(film => {
-      if (film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)) {
-        count++;
-      }
-    });
-    console.log(count);
-  } else {
-    console.error(error);
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    return;
+  }
+
+  try {
+    const data = JSON.parse(body);
+    count = data.results.reduce((total, film) => {
+      const characterFound = film.characters.some(character => character.includes(characterId));
+      return total + (characterFound ? 1 : 0);
+    }, 0);
+    console.log('Number of movies:', count);
+  } catch (parseError) {
+    console.error('Parsing error:', parseError);
   }
 });
