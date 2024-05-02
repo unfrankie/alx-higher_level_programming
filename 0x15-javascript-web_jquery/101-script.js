945ede5b3f94b6ba7192ede9bf9061470a8d1617
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-05-02 15:06:37 +0100

    Create 101-script.js

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,9 @@
+$('#add_item').click(function() {
+    $('ul.my_list').append('<li>Item</li>');
+});
+$('#remove_item').click(function() {
+    $('ul.my_list li:last-child').remove();
+});
+$('#clear_list').click(function() {
+    $('ul.my_list').empty();
+});
