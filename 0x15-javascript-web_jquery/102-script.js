ae86b57e62b143f9d668beeee0d30d13f331f51a
unfrankie <141495574+unfrankie@users.noreply.github.com>
2024-05-02 15:07:38 +0100

    Create 102-script.js

---
+++ 0x15-javascript-web_jquery/102-script.js
@@ -0,0 +1,6 @@
+$('#btn_translate').click(function() {
+    const langCode = $('#language_code').val();
+    $.get('https://www.fourtonfish.com/hellosalut/hello/?lang=' + langCode, function(data) {
+        $('#hello').text(data.hello);
+    });
+});
